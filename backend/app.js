687ea453fc34d7b9e4797dfda5a131031bb91bf5
ayush <ayushvadodariya12@gamil.com>
2025-02-08 10:55:23 +0530

    Initialize nodejs backend and react frontend with basic tailwind setup

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { authRouter } from './routes/index.js';
+import { connectMongoDb } from './connection.js';
+import { authMiddleware } from './middlewares/authentication.js';
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+await connectMongoDb(process.env.DB_URI)
+
+app.use('/api/v1/auth', authRouter);
+
+app.listen(PORT, ()=>{console.log(`Server started at PORT ${PORT}`)});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className=" bg-blue-300 font-bold text-3xl">Bill Generator</div>
+  )
+}
+
+export default App
